60d6a007270aba2d57829a586ff82dd41edd76ea
jestrada-2023074 <jestrada-2023074@kinal.edu.gt>
2024-07-08 16:53:31 -0600

    Se agrega la entidad Proveedores y sus atributos

--- DBMobiExpress.sql
+++ DBMobiExpress.sql
@@ -12,3 +12,13 @@ create table Clientes(
     telefonoCliente varchar(8) not null,
     primary key PK_codigoCliente (codigoCliente)
 );
+create table Proveedores(
+	codigoProveedor int not null auto_increment,
+    NITProveedor varchar(10) not null,
+    nombreProveedor varchar(60) not null,
+    apellidoProveedor varchar(60) not null,
+    razonSocial varchar(60) not null,
+    contactoPrincipal varchar(100) not null,
+    paginaWeb varchar(50) not null,
+    primary key PK_codigoProveedor (codigoProveedor)
+);
