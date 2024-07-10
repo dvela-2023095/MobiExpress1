31259edcfcff9e6a63180f4b0a059778c71bb26d
jestrada-2023074 <jestrada-2023074@kinal.edu.gt>
2024-07-10 15:39:31 -0600

    Se agrega Proveedores y Proveedores.jsp

---
+++ MobiExpress/src/java/modelo/Clientes.java
@@ -0,0 +1,73 @@
+package modelo;
+
+
+public class Clientes {
+    private int codigoCliente;
+    private String NITCliente;
+    private String nombresCliente;
+    private String apellidosCliente;
+    private String direccionCliente;
+    private String telefonoCliente;
+
+    public Clientes() {
+    }
+
+    public Clientes(int codigoCliente, String NITCliente, String nombresCliente, String apellidosCliente, String direccionCliente, String telefonoCliente) {
+        this.codigoCliente = codigoCliente;
+        this.NITCliente = NITCliente;
+        this.nombresCliente = nombresCliente;
+        this.apellidosCliente = apellidosCliente;
+        this.direccionCliente = direccionCliente;
+        this.telefonoCliente = telefonoCliente;
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNITCliente() {
+        return NITCliente;
+    }
+
+    public void setNITCliente(String NITCliente) {
+        this.NITCliente = NITCliente;
+    }
+
+    public String getNombresCliente() {
+        return nombresCliente;
+    }
+
+    public void setNombresCliente(String nombresCliente) {
+        this.nombresCliente = nombresCliente;
+    }
+
+    public String getApellidosCliente() {
+        return apellidosCliente;
+    }
+
+    public void setApellidosCliente(String apellidosCliente) {
+        this.apellidosCliente = apellidosCliente;
+    }
+
+    public String getDireccionCliente() {
+        return direccionCliente;
+    }
+
+    public void setDireccionCliente(String direccionCliente) {
+        this.direccionCliente = direccionCliente;
+    }
+
+    public String getTelefonoCliente() {
+        return telefonoCliente;
+    }
+
+    public void setTelefonoCliente(String telefonoCliente) {
+        this.telefonoCliente = telefonoCliente;
+    }
+    
+    
+}
