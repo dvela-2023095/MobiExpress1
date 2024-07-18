ebeb30409dae08776a075a969c9f2e457b274413
sveliz2023124 <sveliz-2023124@kinal.edu.gt>
2024-07-17 21:42:24 -0600

    Se agrega CRUD de Clientes

--- MobiExpress/src/java/modelo/ClientesDAO.java
+++ MobiExpress/src/java/modelo/ClientesDAO.java
@@ -5,6 +5,8 @@ import config.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ClientesDAO {
@@ -12,6 +14,7 @@ public class ClientesDAO {
     Connection con;
     PreparedStatement ps;
     ResultSet rs;
+    int resp;
     
     public Clientes validar (String usuario, String passwor){
         Clientes cliente = new Clientes();
@@ -32,5 +35,103 @@ public class ClientesDAO {
             e.printStackTrace();
         }
         return cliente;
+    }
+    
+    public List lista(){
+        String sql = "select * from Clientes";
+        Clientes cliente = new Clientes();
+        List<Clientes> listaCliente = new ArrayList<>();
+        try{
+            con = cn.Conexion();
+            ps = con.prepareCall(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+             cliente.setCodigoCliente(rs.getInt(1));
+             cliente.setNITCliente(rs.getString(2));
+             cliente.setNombresCliente(rs.getString(3));
+             cliente.setApellidosCliente(rs.getString(4));
+             cliente.setDireccionCliente(rs.getString(5));
+             cliente.setTelefonoCliente(rs.getString(6));
+             cliente.setUsuario(rs.getString(7));
+             cliente.setPasswor(rs.getString(8));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return listaCliente;
+    }
+    
+    public int agregar(Clientes cliente){
+        String sql = "Insert into Clientes(NITCliente, nombresCliente, apellidosCliente, direccionCliente, telefonoCliente, "
+                + "usuario, passwor) values (?,?,?,?,?,?,?)";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNITCliente());
+            ps.setString(2, cliente.getNombresCliente());
+            ps.setString(3, cliente.getApellidosCliente());
+            ps.setString(4, cliente.getDireccionCliente());
+            ps.setString(5, cliente.getTelefonoCliente());
+            ps.setString(6, cliente.getUsuario());
+            ps.setString(7, cliente.getPasswor());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return resp;
+    }
+    
+    public Clientes buscarcodigoCliente(int id){
+        String sql = "select * from Clientes where codigoCliente = ?";
+        Clientes cliente = new Clientes();
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                cliente.setCodigoCliente(rs.getInt(1));
+                cliente.setNITCliente(rs.getString(2));
+                cliente.setNombresCliente(rs.getString(3));
+                cliente.setApellidosCliente(rs.getString(4));
+                cliente.setDireccionCliente(rs.getString(5));
+                cliente.setTelefonoCliente(rs.getString(6));
+                cliente.setUsuario(rs.getString(7));
+                cliente.setPasswor(rs.getString(8));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return cliente;
+    }
+    
+    public int actualizarCliente(Clientes cliente){
+        String sql = "Update Cliente set NITCliente = ?, nombresCliente = ?, apellidosCliente = ?, direccionCliente = ?"
+                + "  telefonoCliente = ?, usuario = ?, passwor = ?";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNITCliente());
+            ps.setString(2, cliente.getNombresCliente());
+            ps.setString(3, cliente.getApellidosCliente());
+            ps.setString(4, cliente.getDireccionCliente());
+            ps.setString(5, cliente.getTelefonoCliente());
+            ps.setString(6, cliente.getUsuario());
+            ps.setString(7, cliente.getPasswor());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return resp;
+    }
+    
+    public void eliminarCliente(int id){
+        String sql = "delete from Clientes where codigoCliente = ?";
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
